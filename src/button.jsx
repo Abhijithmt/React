464e54a51630ae7463293be2a80047caccb61b97
abhijith <abhijithmtmkm@gmail.com>
2026-02-17 15:10:22 +0530

    basic onlick

--- src/button.jsx
+++ src/button.jsx
@@ -1,4 +1,6 @@
 function Button(){
-    return(<button className="button">About me</button>)
+    const msgwhenclicking= () => console.log("click me");
+    
+    return(<button  onClick={msgwhenclicking}className="button">About me</button>)
 }
 export default Button
