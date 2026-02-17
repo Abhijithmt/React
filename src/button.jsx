5ff87e55b9961ca5dc492a5255d9cbb2a1ae5ca3
abhijith <abhijithmtmkm@gmail.com>
2026-02-17 15:16:55 +0530

    giving conditions when clicking

--- src/button.jsx
+++ src/button.jsx
@@ -1,6 +1,18 @@
 function Button(){
-    const msgwhenclicking= () => console.log("click me");
+
+    let count=0
+
+    const msgwhenclicking= (name) => {
+        if(count < 5){
+            count ++
+            console.log(`${name} you have Click ${count}`); 
+        }else{
+            console.log(`STOP CLICKING${name}`);
+            
+        }
+
+    }
     
-    return(<button  onClick={msgwhenclicking}className="button">About me</button>)
+    return(<button  onClick={ () => msgwhenclicking("abhi")}className="button">About me</button>)
 }
 export default Button
