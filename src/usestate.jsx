36932677fcbaeba61a8494cead05a63979e314a1
abhijith <abhijithmtmkm@gmail.com>
2026-02-18 13:14:14 +0530

    increment age

--- src/usestate.jsx
+++ src/usestate.jsx
@@ -1,19 +1,31 @@
 import { useState } from "react"
  export default function Usestates(){
 
-    const[name,setName]=useState()
+    const[name,setName]=useState("guest")
+
+    const [age,setAge]=useState(0)
 
 
     const upatename = () =>{
-        setName("abhijith")
-
+        setName("abhiiii")
     }
+    
+        const incremenage= ()=>{
+            setAge(age + 1)
+        }
+
 
     return(
         <div>
         <p>Name:{name}</p>
         <button onClick={upatename}>update name</button>
+
+
+
+        <p>age:{age}</p>
+        <button onClick={incremenage}>increment age</button>
         </div>
+        
     
     )
 
